be06e530d57bc7cacaba819991522252781a18b2
rohit <rohiitrb@gmail.com>
2026-01-20 15:28:18 +0530

    avatar updated

--- src/lib/imageLink.ts
+++ src/lib/imageLink.ts
@@ -1,3 +1,3 @@
-export const RohitAvatar = "https://pbs.twimg.com/profile_images/1944049924502695940/0P77msmD_400x400.jpg"
+export const RohitAvatar = "https://pbs.twimg.com/profile_images/2009850117755740160/s4wDoFf4_400x400.jpg"
 
 export const BlueTick = "pnpm install lucide-react"
